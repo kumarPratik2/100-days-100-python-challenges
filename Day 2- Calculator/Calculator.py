298a8197655ad8ca5df3b7a1f9a0ec98979a9118
Pratik Kumar <pratiktkumar@gmail.com>
2026-05-24 23:33:59 +0530

    Add files via upload

--- Day 2- Calculator/Calculator.py
+++ Day 2- Calculator/Calculator.py
@@ -1,29 +1,33 @@
+#definig addition function
 def add(a,b):
     print(f'Sum of given numbers is: {a+b}')
 
+#definig subtraction function
 def sub(a,b):
     print(f'Difference of given numbers is: {a-b}')
 
+#definig multiplication function
 def mul(a,b):
     print(f'Product of given numbers is: {a*b}')
 
+#definig division function
 def div(a,b):
-    print(f'Divison of given numbers is: {a/b}')
+    print(f'Division of given numbers is: {a/b}')
 
-
-a = input('Enter first nmber: ')
+#taking inputs and storing them as float for calculations
+a = input('Enter first number: ')
 b = input('Enter second number: ')
 
 a,b = float(a), float(b)
 
     
-
+#task will check if user has given valid operation or not
 task = True
             
 while task == True:
 
 
-    print('"add" for Addition, "sub" for Substraction, "mul" for Multiplication, "div" for Divison')
+    print('"add" for Addition, "sub" for Subtraction, "mul" for Multiplication, "div" for Division')
     x = input('Choose the function you want to perform: ')
     if x == 'add':
         add(a,b)
@@ -39,6 +43,7 @@ while task == True:
     else:
         print('Enter valid function: ')
 
+#s will check if user has given valid input(y/n) or not
     s = False
     while s == False:
         z =input('Type "y" if you have another operation or "n" if not: ' )
